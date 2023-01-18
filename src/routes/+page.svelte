92a3e381e60108c75793b666a23596275ca303d1
Fredrik Frodlund <fredrik@frodlund.se>
2023-01-18 20:27:17 +0100

    Add form for adding switches

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,37 @@
-<script>
+<script lang="ts">
   import '../styles/base.css';
+  import { onMount } from 'svelte';
+
   import Switch from '$lib/components/Switch.svelte';
+
+  let switches: any = [];
+  let savedSwitches: any = [];
+  let selected: boolean;
+
+  onMount(() => {
+    savedSwitches = localStorage.getItem('switches');
+    switches = JSON.parse(savedSwitches) || [];
+  });
+
+  const insertSwitch = () => {
+    switches = [...switches, selected];
+    localStorage.setItem('switches', JSON.stringify(switches));
+    console.log(selected);
+  };
 </script>
 
 <h1>Switch Port Utilization Tool™</h1>
 <p>Built using Sveltekit by someone that wants to learn more. 😘</p>
 
-<Switch numberOfPorts={16} />
+<form on:submit|preventDefault={insertSwitch}>
+  <select id="switchsize" bind:value={selected}>
+    <option value="16">16</option>
+    <option value="24">24</option>
+    <option value="48">48</option>
+  </select>
+  <button type="submit">Add switch</button>
+</form>
 
-<Switch numberOfPorts={24} />
-
-<Switch numberOfPorts={48} />
+{#each switches as item}
+  <Switch numberOfPorts={item} />
+{/each}
