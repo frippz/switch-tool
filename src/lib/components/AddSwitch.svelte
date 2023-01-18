897a93eff9624deadea8fc5f69163b7eb85e75a9
Fredrik Frodlund <fredrik@frodlund.se>
2023-01-18 20:06:44 +0100

    Create AddSwitch component

---
+++ src/lib/components/AddSwitch.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let onSubmit: (selected: number) => void;
+
+  let selected: number;
+
+  const submitSwitchConfig = () => {
+    onSubmit(selected);
+  };
+</script>
+
+<form on:submit|preventDefault={submitSwitchConfig}>
+  <select id="switchsize" bind:value={selected}>
+    <option value="16">16</option>
+    <option value="24">24</option>
+    <option value="48">48</option>
+  </select>
+  <button type="submit">Add switch</button>
+</form>
